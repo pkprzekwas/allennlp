bc80f200d11a16ebb59de96412afd95d8595cc6d
Malo Marrec <malo@stanford.edu>
2018-05-29 10:07:20 -0700

    add better path

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import subprocess
 
-bash_cd ="python3 -m allennlp.run train tutorials/getting_started/simple_tagger.json --serialization-dir /tmp/tutorials/getting_started -s '/logs'"
+bash_cd ="python3 -m allennlp.run train tutorials/getting_started/simple_tagger.json --serialization-dir /tmp/tutorials/getting_started -s '/logs/outputs'"
 
 subprocess.call(bash_cd, shell=True)
