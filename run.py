e28e72c12ba5037033ae70c9063b559a89bd5d90
Malo Marrec <malo@stanford.edu>
2018-05-29 08:31:08 -0700

    add serialization dir

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import subprocess
 
-bash_cd ="python3 -m allennlp.run train tutorials/getting_started/simple_tagger.json --serialization-dir /tmp/tutorials/getting_started --recover"
+bash_cd ="python3 -m allennlp.run train tutorials/getting_started/simple_tagger.json --serialization-dir /tmp/tutorials/getting_started -s '/logs'"
 
 subprocess.call(bash_cd, shell=True)
