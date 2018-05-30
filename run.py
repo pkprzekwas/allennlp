8bc5006563c564131c8b8a80fb1a3b4f238768ad
Malo Marrec <malo@stanford.edu>
2018-05-30 14:21:36 -0700

    add script

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import subprocess
 
-bash_cd ="python3 -m allennlp.run train tutorials/getting_started/simple_tagger.json --serialization-dir /tmp/tutorials/getting_started -s '/logs/outputs'"
+bash_cd ="python3 -m allennlp.run evaluate https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz --evaluation-data-file https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json"
 
 subprocess.call(bash_cd, shell=True)
