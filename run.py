29b1e3380716be49961f4bf26de57694cd990221
Malo Marrec <malo@stanford.edu>
2018-05-30 15:27:09 -0700

    train + eval allennlp

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
 import subprocess
+import os, argparse
 
-bash_cd ="python3 -m allennlp.run evaluate https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz --evaluation-data-file https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json"
+EVALUATE ="python3 -m allennlp.run evaluate https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz --evaluation-data-file https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json"
 
-subprocess.call(bash_cd, shell=True)
+TRAIN ="python3 -m allennlp.run train training_config/bidaf.json -s '/logs'"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", default="train", help="train or evaluate")
+args = parser.parse_args()
+
+if args.mode == "train":
+    subprocess.call(TRAIN, shell=True)
+elif args.mode =="evaluate":
+    subprocess.call(EVALUATE, shell=True)
+else:
+    raise(NotImplementedError)
+
+
+print(args.mode)
+
+
+#
