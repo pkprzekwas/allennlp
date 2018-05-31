acf0b5f329b8af1cee137126f1ba7013ab11d578
Malo Marrec <malo@stanford.edu>
2018-05-30 19:00:07 -0700

    Tweak serdir to not conflict with /logs

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os, argparse
 
 EVALUATE ="python3 -m allennlp.run evaluate https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz --evaluation-data-file https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json"
 
-TRAIN ="python3 -m allennlp.run train training_config/bidaf.json -s '/logs'"
+TRAIN ="python3 -m allennlp.run train training_config/bidaf.json -s '/logs/outputs'"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", default="train", help="train or evaluate")
