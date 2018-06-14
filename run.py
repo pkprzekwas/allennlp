2f2666c69164f1523b5c757c2048f969d76a8c87
Patryk Przekwas <patryk@clusterone.com>
2018-06-14 17:45:41 +0200

    Changed output directory

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import subprocess
 
-TRAIN_CPU ="python3 -m allennlp.run train training_config/bidaf_cpu.json -s '/logs/outputs'"
+TRAIN_CPU ="python3 -m allennlp.run train training_config/bidaf_cpu.json -s './logs/outputs'"
 subprocess.call(TRAIN_CPU, shell=True)
